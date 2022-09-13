292dce2d634c58cf04a768d8e1238b9f32ac0f25
Ryan | byldr.eth <rtcx86@gmail.com>
2022-09-13 15:17:06 -0500

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ function Header() {
   return (
     <header className="container flex text-center mx-auto py-10">
       <div className="md:w-4/6 mx-auto">
-        <img src="/ryan-selfie-4-500px.jpeg" className="h-24 rounded-full mx-auto mb-5" />
+        <img src="/humphries-profile-photo.jpeg" className="h-24 rounded-full mx-auto mb-5" />
         <h1 className="font-bold">Ryan Humphries</h1>
         <p><a className="hover:text-gray-600" href="https://etherscan.io/address/0xbacon.eth">0xbacon.eth</a></p>
       </div>
